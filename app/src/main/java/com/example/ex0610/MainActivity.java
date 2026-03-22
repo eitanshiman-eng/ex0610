59a972b2d4073a9e838ea9854045167b5e22f89d
eitan <eitanshiman@gmail.com>
2026-03-23 00:32:48 +0200

    First

---
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ex0610;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch sw;
+    private ToggleButton tb;
+    private Button btn;
+    private LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    
