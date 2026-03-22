5e7e4ffdb9fb06998fa0c53b7fb0911f69df9fff
eitan <eitanshiman@gmail.com>
2026-03-23 00:35:47 +0200

    Second

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -21,4 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    
+        sw = findViewById(R.id.mySwitch);
+        tb = findViewById(R.id.myToggleButton);
+        btn = findViewById(R.id.btnClick);
+        layout = findViewById(R.id.mainLayout);
+
+    }
+}
